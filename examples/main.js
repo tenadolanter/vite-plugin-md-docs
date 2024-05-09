8aa843b042acee5972e7a264f0c4a95dbf20c100
tenado <690784374@qq.com>
2024-05-09 14:10:15 +0800

    1

--- examples/main.js
+++ examples/main.js
@@ -4,6 +4,14 @@ import router from "./router";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import './styles/common.css';
+import './styles/highlight.css';
+import hljs from 'highlight.js';
+router.afterEach(route => {
+  Vue.nextTick(() => {
+    const blocks = document.querySelectorAll('pre code:not(.hljs)');
+    Array.prototype.forEach.call(blocks, hljs.highlightBlock);
+  });
+});
 Vue.use(ElementUI);
 new Vue({
   router,
