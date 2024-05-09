8014448c649ccd8bd4c4358ec6666d4c6f31110c
tenado <690784374@qq.com>
2024-05-09 13:58:28 +0800

    1

--- src/markdown/container.js
+++ src/markdown/container.js
@@ -1,7 +1,7 @@
 /**
  * markdown-it-container用于在md文档中创建自定义容器
  *
-*/
+ */
 const MarkdownContainer = require("markdown-it-container");
 module.exports = (options) => {
   const { startTag, endTag, componentName } = options || {};
@@ -16,12 +16,20 @@ module.exports = (options) => {
           const description = m && m.length > 1 ? m[1] : "";
           const content =
             tokens[idx + 1].type === "fence" ? tokens[idx + 1].content : "";
-          return `<${componentName}>
+          return `<div class="demo-block" style="border: solid 1px #ebebeb;border-radius: 3px;transition: .2s;margin-bottom: 24px;">
           ${description ? `<div>${markdown.render(description)}</div>` : ""}
           ${startTag} ${content}${endTag}
+          <div class="meta">
+            <div class="highlight">
+            <pre><code class="html">${content}</code></pre>
+            </div>
+          </div>
+          <div class="demo-block-control" style="text-aligin: center;">
+            <span>显示隐藏代码</span>
+          </div>
           `;
         }
-        return `</${componentName}>`;
+        return `</div>`;
       },
     });
     markdown.use(MarkdownContainer, "tip");
