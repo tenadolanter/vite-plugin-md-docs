0b041b1592ada49c8458ced4a02488b43239d583
tenado <690784374@qq.com>
2024-05-28 21:30:14 +0800

    1

--- vite.example.js
+++ vite.example.js
@@ -1,19 +1,21 @@
 import VitePluginMdDocs from "./src/index.js";
 import Vue from "@vitejs/plugin-vue2";
+import commonjs from "vite-plugin-commonjs";
 import { resolve } from "path";
 export default () => {
   return {
-    base: './',
+    base: "./",
     build: {
       outDir: "dist",
       rollupOptions: {
-        input: resolve(__dirname, 'index.html'),
+        input: resolve(__dirname, "index.html"),
       },
       target: "es2015",
     },
     plugins: [
       VitePluginMdDocs({ frame: "vue" }),
       Vue({ include: [/(\.vue)$/, /\.md$/] }),
+      commonjs(),
     ],
     resolve: {
       alias: {
--- vite.lib.js
+++ vite.lib.js
@@ -1,17 +1,18 @@
-
 import { resolve } from "path";
+import commonjs from "vite-plugin-commonjs";
 export default () => {
   return {
     build: {
       lib: {
-        entry: resolve(__dirname, 'src/index.js'),
-        name: 'VitePluginMdDocs',
-        fileName: 'index',
+        entry: resolve(__dirname, "src/index.js"),
+        name: "VitePluginMdDocs",
+        fileName: "index",
       },
       outDir: "lib",
       // 库模式下不单独拆分css了
       cssCodeSplit: false,
     },
+    plugins: [commonjs()],
     resolve: {
       alias: {
         "@src": resolve("./src"),
